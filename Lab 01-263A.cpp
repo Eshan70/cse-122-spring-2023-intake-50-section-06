0b0139dc149dfbf756a12ffaebf2a29ae92c1ddc
Eshan Munshi <122246307+Eshan70@users.noreply.github.com>
2023-02-13 10:48:21 +0600

    Create Lab 01-263A.cpp

---
+++ Lab 01-263A.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main()
+{
+    int i,j,m=0,n=0;
+    char a[5];
+    for(i=0;i<5;i++){
+        for(j=0;j<5;j++){
+    cin>>a[j];
+            if(a[j]=='1'){
+            m=i;
+            n=j;}
+    }}
+    cout<<abs(m-2)+abs(n-2);
+}
